0a97134ff56b0f0e731177b88c24b8b28e57b138
giulio30795 <giulioincarbone95@gmail.com>
2022-01-21 16:48:32 +0100

    check

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,7 +10,6 @@ class MovieController extends Controller
     public function index(){
         $movies = Movie::all();
 
-        dump($movies);
         return view('movies', compact('movies'));
     }
 }
